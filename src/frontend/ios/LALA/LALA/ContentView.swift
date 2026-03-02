cb73c547b7b9365d540f28a39a98351fa197d9c7
RudinP <song_e@sookmyung.ac.kr>
2026-03-02 11:53:40 +0900

    feat(frontend(ios)): initialize xcode ios app

---
+++ src/frontend/ios/LALA/LALA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LALA
+//
+//  Created by 루딘 on 3/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ src/frontend/ios/LALA/LALA/LALAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LALAApp.swift
+//  LALA
+//
+//  Created by 루딘 on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LALAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
